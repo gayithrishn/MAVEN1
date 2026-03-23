2f2d915dc23e077f2f2b8a2bf39bd242363e45eb
karthik <karthik123@gmail.com>
2026-03-23 10:36:38 +0000

    second commit

--- src/main/java/com/mvit/App.java
+++ src/main/java/com/mvit/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("jenkins it is an CI/CD tool");
     }
 }
